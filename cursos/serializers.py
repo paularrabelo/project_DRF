47e618060ac54229cf9e37813f12cd7181c17f2f
Paula Rabelo <paula_rabelo2010@hotmail.com>
2024-10-04 10:22:45 -0300

    Utilizando o Primary Key Related Field para criar relacoes

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -27,7 +27,10 @@ class CursoSerializer(serializers.ModelSerializer):
     #avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
     
     #HyperLinked Related Field
-    avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
+    #avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
+
+    # Primary Key Related Field
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Curso
