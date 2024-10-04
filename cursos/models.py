2248460dbe9d1988b05625c74d476a5ac108cbdb
Paula Rabelo <paula_rabelo2010@hotmail.com>
2024-10-04 11:50:27 -0300

    Adicionando paginação na API

--- cursos/models.py
+++ cursos/models.py
@@ -16,6 +16,7 @@ class Curso(Base):
     class Meta:
         verbose_name = 'Curso'
         verbose_name_plural = 'Cursos'
+        ordering = ['id']
 
     def __str__(self):
         return self.titulo
@@ -26,11 +27,13 @@ class Avaliacao(Base):
     email = models.EmailField()
     comentario = models.TextField(blank=True, default='')
     avaliacao = models.DecimalField(max_digits=2, decimal_places=1)
+    
 
     class Meta:
         verbose_name = 'Avaliação'
         verbose_name_plural = 'Avaliações'
         unique_together = ['email', 'curso']
+        ordering = ['id']
 
     def __str__(self):
         return f'{self.nome} avaliou o curso {self.curso} com nota {self.avaliacao}'
