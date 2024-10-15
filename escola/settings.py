e9517e3effc567d02341952b5934e1c606003799
Paula Rabelo <paula_rabelo2010@hotmail.com>
2024-10-15 10:46:43 -0300

    Criando e configurando o docker-compose

--- escola/settings.py
+++ escola/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "escola.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE" ,BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password") ,
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
@@ -151,3 +155,13 @@ REST_FRAMEWORK = {
         'user': '10/minute'
     }
 }
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = int(os.environ.get("DEBUG", default=0))
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
