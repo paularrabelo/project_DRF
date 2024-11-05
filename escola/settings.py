65c50dc7fa3593a89329b80248da2a35942789e2
Paula Rabelo <paula_rabelo2010@hotmail.com>
2024-11-05 10:33:19 -0300

    Atualização completa do código para teste

--- escola/settings.py
+++ escola/settings.py
@@ -136,10 +136,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        #'rest_framework.authentication.SessionAuthentication', - Autenticação via Sessão
-        'rest_framework.authentication.TokenAuthentication',
-    ),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     #'rest_framework.authentication.SessionAuthentication', - Autenticação via Sessão
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
